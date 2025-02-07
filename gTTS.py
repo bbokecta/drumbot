b824f3c2be41ff94f963aabe386cc9193e373ef7
Bami <bami.oke@hogarth.com>
2025-02-07 13:34:19 +0000

    changed pitch of gtts

--- gTTS.py
+++ gTTS.py
@@ -1,12 +1,25 @@
 import os
 from gtts import gTTS
 from playsound import playsound
+from pydub.effects import speedup
+from pydub import AudioSegment
+
+import soundfile as sf
+import pyrubberband as pyrb
 
 def get_speech(answer):
     tts = gTTS(answer, lang='en', tld="com.ng")
     speech_file_path = os.path.join("voices/", "audio.mp3")
     tts.save(speech_file_path)
 
-    # subprocess.run(['ffmpeg', '-y' '-i', 'voices/initial_audio.mp3', '-filter:a', "atempo=5.0", 'voices/audio.mp3'])
+    sound = AudioSegment.from_file(speech_file_path)
+    octaves = -0.75
+    # for octaves in np.linspace(-1,1,21):
+    new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+    hipitch_sound = sound._spawn(sound.raw_data, overrides=
+                                    {'frame_rate': new_sample_rate})
+    hipitch_sound = hipitch_sound.set_frame_rate(44100)
+    hipitch_sound.export(speech_file_path, format="mp3")
+
     playsound(speech_file_path) #disable this when streaming to Omniverse
     os.remove(speech_file_path)
